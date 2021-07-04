99e278584f233537f1572ca7ba57bf3f6983e7d1
SpasPetrov <spaspetrov23@gmail.com>
2021-07-04 17:36:13 +0300

    Initial upload.

--- SheetsWithoutNumber/SWN.Data/DataConstants.cs
+++ SheetsWithoutNumber/SWN.Data/DataConstants.cs
@@ -5,8 +5,6 @@
         public const int UsernameMaxLength = 30;
         public const string UserEmailRegularExpression = @"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$";
 
-
-
         public const int GameNameMaxLength = 50;
         public const int GameDescriptionMaxLength = 200;
     }
