43b0e56f272fd35edfbd9984a075399cce5c6573
SpasPetrov <spaspetrov23@gmail.com>
2021-07-08 00:42:35 +0300

    Included a WIP Registration form MVC.

---
+++ SheetsWithoutNumber/SheetsWithoutNumber/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+﻿namespace SheetsWithoutNumber.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using SheetsWithoutNumber.Models.Users;
+    using SWN.Data;
+    using SWN.Data.Models;
+    using System;
+
+    public class UsersController : Controller
+    {
+        private readonly ApplicationDbContext data;
+
+        public UsersController(ApplicationDbContext data)
+        {
+            this.data = data;
+        }
+
+        public IActionResult Register() => View();
+
+        [HttpPost]
+        public IActionResult Register(RegisterUserFormModel model)
+        {
+            var user = new User
+            {
+                Username = model.Username,
+                Password = model.Password,
+                Email = model.Email,
+                JoinDate = DateTime.Now
+            };
+
+            data.Users.Add(user);
+
+            data.SaveChanges();
+
+            return Redirect("/");
+        }
+    }
+}
---
+++ SheetsWithoutNumber/SheetsWithoutNumber/Models/Users/RegisterFormUserModel.cs
@@ -0,0 +1,15 @@
+﻿namespace SheetsWithoutNumber.Models.Users
+{
+    using System;
+
+    public class RegisterUserFormModel
+    {
+        public string Username { get; init; }
+
+        public string Email { get; init; }
+
+        public string Password { get; init; }
+
+        public string ConfirmPassword { get; init; }
+    }
+}
